ff884b351f321bc725f2e28e94f7564b1803b52c
enricadirado <100517394+enricadirado@users.noreply.github.com>
2023-06-09 23:29:02 +0200

    1 file modified

--- src/root/inserimento/inserimento.component.ts
+++ src/root/inserimento/inserimento.component.ts
@@ -1,20 +1,26 @@
 import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
 @Component({
   selector: 'app-inserimento',
   templateUrl: './inserimento.component.html',
   styleUrls: ['./inserimento.component.css'],
-  standalone:true,
-  imports: [CommonModule]
+  standalone: true,
+  imports: [CommonModule],
 })
 export class InserimentoComponent implements OnInit {
-  @Input() selezione: string|undefined;
-  constructor() { }
+  @Input() selezione: string | undefined;
+  constructor() {}
   ngOnInit() {}
   clean() {
     this.selezione = undefined;
   }
 }
 
+/*
+view: string= 'home';
+  seleziona(name: string) {
+    this.selezione = name;
+  }
+
+  */
