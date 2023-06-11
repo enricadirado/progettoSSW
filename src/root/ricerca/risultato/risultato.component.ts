621753a3f601e6e8dc3a661d629f0ae5e736cd8d
enricadirado <100517394+enricadirado@users.noreply.github.com>
2023-06-12 01:02:40 +0200

    1 file modified

--- src/root/ricerca/risultato/risultato.component.ts
+++ src/root/ricerca/risultato/risultato.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit , Input} from '@angular/core';
+
 import { PrestitoComponent } from './prestito/prestito.component';
 import { RimozioneComponent } from './rimozione/rimozione.component';
 
@@ -7,10 +9,10 @@ import { RimozioneComponent } from './rimozione/rimozione.component';
   templateUrl: './risultato.component.html',
   styleUrls: ['./risultato.component.css'],
   standalone:true,
-  imports: [RimozioneComponent, PrestitoComponent]
+  imports: [RimozioneComponent, PrestitoComponent, CommonModule]
 })
 export class RisultatoComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit() {
