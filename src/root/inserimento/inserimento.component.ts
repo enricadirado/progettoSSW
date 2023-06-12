a959b784b0db3bda69591ff37e73356d897eac2a
enricadirado <100517394+enricadirado@users.noreply.github.com>
2023-06-12 23:52:47 +0200

    1 file modified

--- src/root/inserimento/inserimento.component.ts
+++ src/root/inserimento/inserimento.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArchivioService } from '../archivio.service';
+import { Libro } from '../libro';
 
 @Component({
   selector: 'app-inserimento',
@@ -14,15 +15,20 @@ export class InserimentoComponent implements OnInit {
   @Output() newViewEvent = new EventEmitter<string>();
   constructor(private as: ArchivioService) {}
   ngOnInit() {}
-  cambioView(name:string){
-    this.selezione=name;
+  cambioView(name: string) {
+    this.selezione = name;
     this.newViewEvent.emit(this.selezione);
   }
 
-  inserisciDoc(){
-    let titolo: string = (document.getElementById("titolo") as HTMLInputElement).value;
-    let autore: string = (document.getElementById("autore") as HTMLInputElement).value;
-    let posizione: string = (document.getElementById("posizione") as HTMLInputElement).value;
+  inserisciDoc() {
+    let titolo: string = (document.getElementById('titolo') as HTMLInputElement)
+      .value;
+    let autore: string = (document.getElementById('autore') as HTMLInputElement)
+      .value;
+    let posizione: string = (
+      document.getElementById('posizione') as HTMLInputElement
+    ).value;
+    let libro: Libro = new Libro(titolo, autore, posizione, 'undefined');
   }
 }
 
