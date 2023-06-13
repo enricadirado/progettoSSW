1c059ce8d997cf5fb8701376e92e23ce32509f68
enricadirado <100517394+enricadirado@users.noreply.github.com>
2023-06-14 00:58:57 +0200

    4 files modified

--- src/root/ricerca/ricerca.component.ts
+++ src/root/ricerca/ricerca.component.ts
@@ -18,7 +18,14 @@ import { RimozioneComponent } from './rimozione/rimozione.component';
 export class RicercaComponent implements OnInit {
   @Input() selezione: string;
   @Output() ricercaLibroEvent = new EventEmitter<string>();
-  
+  numeroLibri: number;
+  archivioFinal: Array<Libro>=[];
+  archivioStart:Archivio;
+  libroTrovato: Libro;
+  titolo:string;
+  autore: string;
+  posizione:string;
+  nominativo: string;
   constructor(private as: ArchivioService) { }
   
   ricercaLibro(){
@@ -26,53 +33,37 @@ export class RicercaComponent implements OnInit {
       next: (x: AjaxResponse<any>) =>{
         let input: HTMLInputElement = document.getElementById("res") as HTMLInputElement;
         let y = input.value;
+        
+        let archivioStart: Archivio= new Archivio(JSON.parse(x.response));
 
-        console.log(JSON.parse(x.response));
-        let archivio1: Archivio = new Archivio(JSON.parse(x.response));
-        console.log(archivio1);
-
-        /*var archivio2: Array<Libro> = JSON.parse(x.response);
-        console.log(archivio2, '2 type:', typeof(archivio2));*/
-
-/*
-        const archivio_filtrato= archivio2.filter((el) =>
+        this.archivioFinal = archivioStart.archivio.filter((el) =>
         (el.titolo + el.autore).toLowerCase().includes(y));
-        console.log(archivio_filtrato, "3 type: ", typeof(archivio_filtrato));
-
+        
+        console.log(archivioStart, '2 type:', typeof(archivioStart));
+        console.log(this.archivioFinal, 'final type:', typeof(this.archivioFinal));
+        
         if(y!=""){
-          if (archivio_filtrato.length===1){
-            this.libroTrovato = archivio_filtrato[0];
+          if (this.archivioFinal.length===1){
+            this.libroTrovato = this.archivioFinal[0];
             console.log('LT', this.libroTrovato, "4 type: ", typeof(this.libroTrovato));
-            this.numeroLibri= archivio_filtrato.length;
+            this.numeroLibri= this.archivioFinal.length;
             this.titolo= this.libroTrovato.titolo;
             this.autore= this.libroTrovato.autore;
             this.posizione= this.libroTrovato.posizione;
             this.nominativo= this.libroTrovato.nominativo;
           } else {
-            this.numeroLibri= archivio_filtrato.length;
+            this.numeroLibri= this.archivioFinal.length;
             console.log(this.numeroLibri);
           } 
         } else {
-          console.log("ex", y)
           this.libroTrovato = undefined;
           this.numeroLibri=-1;
-        }*/
+        }
       },
       error: (err) =>
         console.error('Observer got an error: ' + JSON.stringify(err)),
     });
   }
-  numeroLibri: number;
-  libroTrovato: Libro;
-  archivio1: Archivio;
-
-  titolo: string;
-  autore:string;
-  posizione:string;
-  nominativo:string;
-
-  archivio2: Array<Libro>;
-  
   ngOnInit() {
   }
 }
--- src/root/ricerca/rimozione/rimozione.component.ts
+++ src/root/ricerca/rimozione/rimozione.component.ts
@@ -1,23 +1,20 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, Input} from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Libro } from '../../libro';
 import { ArchivioService } from '../../archivio.service';
+import { Archivio } from '../../archivio';
 
 @Component({
   selector: 'app-rimozione',
   templateUrl: './rimozione.component.html',
   styleUrls: ['./rimozione.component.css'],
-  standalone:true,
-  imports: [CommonModule]
+  standalone: true,
+  imports: [CommonModule],
 })
 export class RimozioneComponent implements OnInit {
-  @Input() archivio2: object;
+  
 
-  constructor(as: ArchivioService) { }
-  rimuoviLibro(){
-    console.log('rimozione:', this.archivio2);
-  }
-  ngOnInit() {
-  }
-
+  constructor(as: ArchivioService) {}
+  
+  ngOnInit() {}
 }
