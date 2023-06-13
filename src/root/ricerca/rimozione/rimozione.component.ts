06618d7b56d2b26a9904d44b5d1dcda40f80dedb
enricadirado <100517394+enricadirado@users.noreply.github.com>
2023-06-14 01:22:04 +0200

    3 files modified

--- src/root/ricerca/rimozione/rimozione.component.ts
+++ src/root/ricerca/rimozione/rimozione.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Libro } from '../../libro';
 import { ArchivioService } from '../../archivio.service';
 import { Archivio } from '../../archivio';
+import { AjaxResponse } from 'rxjs/ajax';
 
 @Component({
   selector: 'app-rimozione',
@@ -12,9 +13,25 @@ import { Archivio } from '../../archivio';
   imports: [CommonModule],
 })
 export class RimozioneComponent implements OnInit {
-  
-
-  constructor(as: ArchivioService) {}
-  
+  @Input() libroTrovato: Libro;
+  archivioFinal: Array<Libro>=[];
+  constructor(private as: ArchivioService) {}
+  rimuoviDoc(){
+    console.log('rimozione', this.libroTrovato);
+    this.as.getData().subscribe({
+      next: (x: AjaxResponse<any>) =>{
+        let archivioStart: Archivio= new Archivio(JSON.parse(x.response));
+        this.archivioFinal = archivioStart.archivio.filter((el) =>
+        (el.titolo !== this.libroTrovato.titolo || el.autore !== this.libroTrovato.autore || el.posizione !== this.libroTrovato.posizione || el.nominativo !== this.libroTrovato.nominativo));
+        console.log('final', this.archivioFinal);
+      },
+      error: (err) =>
+        console.error('Observer got an error: ' + JSON.stringify(err)),
+    });
+  }
   ngOnInit() {}
 }
+
+
+
+
