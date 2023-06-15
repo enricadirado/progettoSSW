1d7fe8e595dd2d056742288b83c29fc15f461e36
enricadirado <100517394+enricadirado@users.noreply.github.com>
2023-06-15 23:43:58 +0200

    1 file modified

--- src/root/ricerca/risultato/prestito/prestito.component.ts
+++ src/root/ricerca/risultato/prestito/prestito.component.ts
@@ -12,7 +12,7 @@ import { Archivio } from '../../../archivio';
 })
 export class PrestitoComponent implements OnInit {
   @Input() libroTrovato: Libro;
-  archivioFinal: Array<Libro>=[];
+  archivio2: Array<Libro>=[];
   constructor(private as: ArchivioService) { }
   
   newName(){
@@ -24,26 +24,36 @@ export class PrestitoComponent implements OnInit {
     this.libroTrovato.nominativo=nome;
     console.log('nuovo nominativo:', this.libroTrovato.nominativo);
 
-    /*prendo l'archivio*/
+    
     this.as.getData().subscribe({
       next: (x: AjaxResponse<any>) => {
+        /*prendo l'archivio*/
         let archivioStart: Archivio= new Archivio(JSON.parse(x.response));
         console.log('archivio start', archivioStart);
-        /*elimino il libro*/
-        this.archivioFinal = archivioStart.archivio.filter((el) =>
-        (el.titolo !== this.libroTrovato.titolo || el.autore !== this.libroTrovato.autore || el.posizione !== this.libroTrovato.posizione || el.nominativo !== this.libroTrovato.nominativo));
 
-        /*aggiungo il libro*/
+        /*elimino il libro e ottengo un array*/
+        this.archivio2 = archivioStart.archivio.filter((el) =>
+        (this.libroTrovato.titolo !== el.titolo || this.libroTrovato.autore !== el.autore ||  this.libroTrovato.posizione !== el.posizione));
+        console.log('archivio 2', this.archivio2);
+
+        /*creo il libro*/
         let libro: Libro = new Libro(this.libroTrovato.autore, this.libroTrovato.titolo, this.libroTrovato.posizione, nome);
-        archivioStart.aggiuntaLibro(libro);
 
-        var archivio2 = JSON.stringify(archivioStart.archivio);
-        console.log(archivio2);
-        var archivio3= JSON.stringify(archivio2);
-        console.log('nuovo archivio', archivio3);
+        /*converto array in archivio*/
+        let archivioMid: Archivio= new Archivio(this.archivio2);
+
+        /*aggiungo libro*/
+        archivioMid.aggiuntaLibro(libro);
+        console.log('mid', archivioMid);
+
+        /*converto array in stringa*/
+        var archivioFinal = JSON.stringify(archivioMid.archivio);
+        console.log('final', archivioFinal);
+
+
         /*setto nuovo valore*/
 
-        this.as.setData(archivio3).subscribe({
+        this.as.setData(archivioFinal).subscribe({
           next: (x: AjaxResponse<any>) => {
             console.log(x.response);
           },
